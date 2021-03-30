6f63f0da2982cfc227a5f3555f5f7554372380a0
Davin <davin.hayden@outlook.com>
2021-03-30 12:39:39 +0100

    added db queries to index.js but still not working incojunction with graphiql console

--- connect.js
+++ connect.js
@@ -20,10 +20,31 @@ connection.connect((err) => {
 
 // sql queries - how to call this from GQL ?
 // would need multiple functions, for each query - check format of my basic MongoDB TIL example
-let query1 = 'SELECT q_id, question, gp_order, points_type from ref_core_questions'
-let query2 = "SELECT rcq.q_id, rcq.question, rcq.points_type, rcp.pointsA_not, rcp.pointsB_only, rcp.pointsC_sometimes, rcp.pointsD_often, rcp.pointsE_most FROM ref_core_questions AS rcq INNER JOIN ref_core_points AS rcp ON rcq.points_type = rcp.points_id ORDER BY rcq.gp_order;"
 
-connection.query(query2, (err,rows) => {
+// Basic Query
+let queryGetBasicQuestions = `
+	SELECT q_id, question, gp_order, points_type 
+	FROM ref_core_questions
+	`
+// get questions and their points value, in order to build UI table
+let queryGetAllQuestionDetails = `
+	SELECT rcq.q_id, rcq.question, rcq.points_type, rcp.pointsA_not, rcp.pointsB_only, rcp.pointsC_sometimes, rcp.pointsD_often, rcp.pointsE_most 
+	FROM ref_core_questions AS rcq 
+	INNER JOIN ref_core_points AS rcp 
+		ON rcq.points_type = rcp.points_id 
+	ORDER BY rcq.gp_order;
+	`
+
+let queryGetUsersAnswers = `
+	SELECT u.user_id, u.name, ucs.score_date, ucs.overall_score, uca.q_id, uca.points 
+	FROM user_core_score AS ucs 
+	INNER JOIN user_core_answers AS uca 
+		ON uca.ucs_id = ucs.ucs_id 
+	INNER JOIN users AS u
+		ON u.user_id = ucs.user_id 
+	ORDER BY u.user_id, ucs.score_date;
+	`
+connection.query(queryGetBasicQuestions, (err,rows) => {
   if(err) throw err;
   console.log('Data received from DB:');
   console.log(rows);
@@ -35,9 +56,42 @@ connection.query(query2, (err,rows) => {
 // });
 
 
+//Basic DB Create/Insert Example
+// const author = { name: 'Craig Buckler', city: 'Exmouth' };
+// con.query('INSERT INTO authors SET ?', author, (err, res) => {
+//   if(err) throw err;
+//   console.log('Last insert ID:', res.insertId);
+// });
+
+
+
 //this ends the connection straight away, if this is the only file being run by node
 connection.end((err) => {
   // The connection is terminated gracefully
   // Ensures all remaining queries are executed
   // Then sends a quit packet to the MySQL server.
 });
+
+
+// TO DO LATER - consider using Stored Proecedures?
+
+// Examples to avoid SQL Injection - for later
+
+// use the mysql.escape method
+// con.query(
+//   `SELECT * FROM authors WHERE id = ${mysql.escape(userSubmittedVariable)}`,
+//   (err, rows) => {
+//     if(err) throw err;
+//     console.log(rows);
+//   }
+// );
+
+// // use ? placeholder 
+// con.query(
+//   'SELECT * FROM authors WHERE id = ?',
+//   [userSubmittedVariable],
+//   (err, rows) => {
+//     if(err) throw err;
+//     console.log(rows);
+//   }
+// );
